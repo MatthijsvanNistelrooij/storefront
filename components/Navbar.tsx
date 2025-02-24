1caaa17302c6d8000959900263e3e07e4d466ace
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-02-24 06:40:08 +0100

    clean up commerce.ts v

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between p-5 bg-slate-700 sticky top-0">
       <Link href={"/"} className="block border p-2">
-        Home <span className="text-xs text-gray-400">v1.3</span>
+        Home <span className="text-xs text-gray-400">v1.2</span>
       </Link>
       <Link
         href={`/cart/${extractedCartId}`}
